cf6af592316d65cc242c84e5a888cad42369d7ee
informati <46190977+Matthieusz@users.noreply.github.com>
2025-07-03 13:28:28 +0200

    refactor App.vue to include PostList component

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,9 @@
 <template>
-  <div>App</div>
+  <div id="app">
+    <PostList />
+  </div>
 </template>
+
+<script setup lang="ts">
+import PostList from './components/posts/PostList.vue'
+</script>
